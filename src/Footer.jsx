c555881fc759c49cc933e3233374cb943b5867fc
alessadroc <alex150227@hotmail.com>
2024-12-09 21:53:17 +1100

    Infdev 1.3.3: Updated Contact slightly. Will change further in 1.4

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,10 +2,10 @@
 
 function Footer() {
     return(
-    <>
+    <div>
         <h2>Contribute</h2>
-        <a href="https://github.com/alessadroc/ideal-fortnite" target="_blank">Github</a>
-    </>
+        <a href="https://github.com/alessadroc/ideal-fortnite" target="_blank" className="contactLinks">Github</a>
+    </div>
     )
 }
 
