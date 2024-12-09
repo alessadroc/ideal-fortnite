246b0a71ca46d65f7316b8c194436184619e6989
alessadroc <alex150227@hotmail.com>
2024-12-10 02:11:19 +1100

    Infdev 1.3.4: UI tweaks and moved away from the yellow theme.

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,6 +5,7 @@ function Footer() {
     <div>
         <h2>Contribute</h2>
         <a href="https://github.com/alessadroc/ideal-fortnite" target="_blank" className="contactLinks">Github</a>
+        <a href="https://github.com/alessadroc/ideal-fortnite" target="_blank" className="contactLinks">Send a Dollar</a>
     </div>
     )
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -2,7 +2,9 @@
 
 function Header() {
     return(<>
+    <div class="headerDIV">
         <h1>Create your ideal Fortnite Battle Royale Loot Pool!</h1>
+    </div>
     </>);
 }
 
