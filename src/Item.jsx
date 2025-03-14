9abbb00decd307b2ef06e4444e90cf536f0920f2
alessadroc <alex150227@hotmail.com>
2025-03-14 13:12:23 +1100

    expanded item component

--- src/Item.jsx
+++ src/Item.jsx
@@ -1,7 +1,12 @@
-function Item() {
-    return(
-        <image></image>
-    )
+function Item(source) {
+    return (
+        <img 
+            src={source}
+            width={150}
+            height={150}
+            alt="Item"
+            />
+    );
 }
 
 export default Item;
