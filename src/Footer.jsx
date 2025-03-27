78da441f917746c061b820e48d38762378dee3c9
alessadroc <alex150227@hotmail.com>
2025-03-27 16:15:54 +1100

    Fixed 'font failed to decode' warning.

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@
 function Footer() {
     return(
     <div className="footerDIV">
-        <h2>CoNntribute</h2>
+        <h2>Contribute</h2>
         <ul className="footerList">
             <li>
                 <a href="https://github.com/alessadroc/ideal-fortnite" target="_blank"><img class="contactLogo" src="src\assets\githubLogo.png"></img></a>
